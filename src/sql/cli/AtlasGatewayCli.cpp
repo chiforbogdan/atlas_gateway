4d2ac29126b79ea788d347d2fabff9e2eef41c7c
comihai <coca.mihai5@gmail.com>
2020-04-06 01:56:16 +0300

    Verify that the pair (identity, psk) is valid when inserting it into the database
    Signed-off-by: comihai <coca.mihai5@gmail.com>

--- src/sql/cli/AtlasGatewayCli.cpp
+++ src/sql/cli/AtlasGatewayCli.cpp
@@ -1,23 +1,33 @@
 #include "../AtlasSQLite.h"
 #include "../../logger/AtlasLogger.h"
+#include <boost/regex.hpp>
 
 #define ATLAS_DB_PATH "local.db"
+
+static bool validate_uuid(const std::string& s)
+{
+   static const 
+   boost::regex e("[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[89aAbB][a-f0-9]{3}-[a-f0-9]{12}");
+   return regex_match(s, e);
+}
 
 static void
 print_usage()
 {
-    printf("atlas_gateway_cli --identity <identity> --psk <psk>\n");
+    printf("Invalid arguments.\n"\
+           "Usage: atlas_gateway_cli --identity <identity> --psk <psk>\n"
+           "Identity must be an UUID value, PSK must be a random 44 bytes value\n");
 }
 
 int main(int argc, char** argv)
 {
 
-   if(argc != 5 || strcmp(argv[1],"--identity") || strcmp(argv[3],"--psk"))
-   {
+   if(argc != 5 || strcmp(argv[1],"--identity") || strcmp(argv[3],"--psk")
+                || !validate_uuid(argv[2]) || strlen(argv[4]) != 44) {
       print_usage();
       return -1;
    }
-
+   
    if(!atlas::AtlasSQLite::getInstance().openConnection(ATLAS_DB_PATH)) {
       ATLAS_LOGGER_ERROR("Error opening database!");
       return -1;
