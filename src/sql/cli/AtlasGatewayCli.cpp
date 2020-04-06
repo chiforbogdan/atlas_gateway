f8e75ce12bacf3fe59d49cdc6d663dc5ec3227df
comihai <coca.mihai5@gmail.com>
2020-04-06 16:52:37 +0300

    Check PSK length  against a specific interval - [10,1000] bytes
    Signed-off-by: comihai <coca.mihai5@gmail.com>

--- src/sql/cli/AtlasGatewayCli.cpp
+++ src/sql/cli/AtlasGatewayCli.cpp
@@ -4,6 +4,13 @@
 
 #define ATLAS_DB_PATH "local.db"
 
+namespace {
+
+const int PSK_MIN_LEN = 10;
+const int PSK_MAX_LEN = 1000;
+
+} // anonymous namespace
+
 static bool validate_uuid(const std::string& s)
 {
    static const 
@@ -11,19 +18,28 @@ static bool validate_uuid(const std::string& s)
    return regex_match(s, e);
 }
 
+static bool validate_psk(const std::string& s)
+{
+   size_t len = s.size();
+   if(len >= PSK_MIN_LEN && len <= PSK_MAX_LEN)
+      return true;
+   return false;
+}
+
 static void
 print_usage()
 {
     printf("Invalid arguments.\n"\
            "Usage: atlas_gateway_cli --identity <identity> --psk <psk>\n"
-           "Identity must be an UUID value, PSK must be a random 44 bytes value\n");
+           "Identity must be an UUID value\n"\
+           "PSK must be a random value between 10 and 1000 bytes\n");
 }
 
 int main(int argc, char** argv)
 {
 
    if(argc != 5 || strcmp(argv[1],"--identity") || strcmp(argv[3],"--psk")
-                || !validate_uuid(argv[2]) || strlen(argv[4]) != 44) {
+                || !validate_uuid(argv[2]) || !validate_psk(argv[4])) {
       print_usage();
       return -1;
    }
